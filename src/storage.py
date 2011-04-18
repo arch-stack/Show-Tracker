77d9679faa1f81d22a6ee8b60fa2adc0da181c99
Alexander Godlewski <alex@implode.ca>
2011-04-17 20:40:28 -0400

    Added comments and docstring to storage.py

--- src/storage.py
+++ src/storage.py
@@ -3,10 +3,17 @@ from distutils.dir_util import mkpath
 from shutil import rmtree
 
 class storage:
+    ''' This class handles read/write to a given path '''
+    
     def __init__(self, path):
         self.__path = path
         
     def savedata(self, relativepath, data):
+        ''' Save data to a path relative to the storage path 
+            relativepath is str
+            data is (data)
+        '''
+        
         fullpath = os.path.join(self.__path, relativepath)
         
         if not os.path.exists(os.path.dirname(fullpath)):
@@ -17,12 +24,24 @@ class storage:
         file.close()
         
     def listdir(self, relativepath):
+        ''' List directories in the relative path 
+            relativepath is str
+        '''
+        
         return os.listdir(os.path.join(self.__path, relativepath))
     
     def exists(self, relativepath):
+        ''' Check if the relative path exists 
+            relativepath is str
+        '''
+        
         return os.path.exists(os.path.join(self.__path, relativepath))
     
     def getdata(self, relativepath):
+        ''' Get data from the data at relativepath 
+            relativepath is str
+        '''
+        
         fullpath = os.path.join(self.__path, relativepath)
         data = None
         
@@ -34,6 +53,10 @@ class storage:
         return data
     
     def removedir(self, relativepath):
+        ''' remove a directory
+            relativepath is str
+        '''
+        
         fullpath = os.path.join(self.__path, relativepath)
         
         if os.path.exists(fullpath):
