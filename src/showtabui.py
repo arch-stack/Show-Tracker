60e9a0dc6d8364beb165121fed71e13205581683
Alexander Godlewski <alex@implode.ca>
2011-04-17 20:06:45 -0400

    Added comments and docstrings to showtabui.py

--- src/showtabui.py
+++ src/showtabui.py
@@ -6,6 +6,8 @@ from seasontabui import seasontabui
 from src.dataclasses import seasonmodel
 
 class showtabui(QWidget):
+    ''' The widget inside a tab representing a show '''
+    
     def __init__(self, parent = None):
         QWidget.__init__(self, parent)
         self.ui = Ui_ShowTab()
@@ -16,9 +18,18 @@ class showtabui(QWidget):
         self.episodestatuschanged = pyqtSignal('QString', 'QDateTime', name = 'episodestatuschanged')
                 
     def closetab(self):
+        ''' Close this widget's tab
+            Only call this when the tab has already been added
+        '''
+        
         self.parent().parent().removeTab(self.parent().parent().indexOf(self))
         
     def loadshow(self, show, backend):
+        ''' Setup the tab with the passed show
+            show is src.dataclasses.show
+            backend is src.backend.backend
+        '''
+        
         self.showid = show.id
         self.backend = backend
         
@@ -42,11 +53,16 @@ class showtabui(QWidget):
 
         
     def openseason(self, index):
+        ''' Open a season that is selected
+            index is QModelIndex
+        '''
+        
         row = index.row()
         season = self.ui.seasonlist.model().getseason(row)
 
         add = True
 
+        # Check if there is already a tab open for this season
         for tabnum in range(len(self.ui.tabs)):
             if hasattr(self.ui.tabs.widget(tabnum), 'seasonid') and self.ui.tabs.widget(tabnum).seasonid == season.id:
                 add = False
@@ -61,14 +77,28 @@ class showtabui(QWidget):
             self.ui.tabs.addTab(newtab, 'Season %d' % season.number)
 
     def episodestatuschangedfunc(self, id, date):
+        ''' Handle when a season tab receives a new date from an episode changing
+            id is QString
+            date is QDateTime
+        '''
+        
         self.ui.seasonlist.model().setseasondate(id, date)
         self.emit(SIGNAL('episodestatuschanged(QString, QDateTime)'), self.showid, self.ui.seasonlist.model().getshowdate())
         
     def displayseasonstatus(self, showid, seasonid):
+        ''' Display the season status for an individual season
+            This only needs to be called when the show tab is opened
+            showid is str
+            seasonid is str
+        '''
+        
         seasondate = None
         episodes = self.backend.getlocalepisodes(showid, seasonid)
         
         for episode in episodes:
+            # Only choose a date if the episode is not watched
+            #     and the date is less than the previously
+            #     selected one
             if seasondate != None:
                 if not episode.watched and episode.date != None:
                     if episode.date < seasondate:
