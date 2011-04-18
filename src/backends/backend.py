52bba906aa18b51ac260a916e413d8d9e79c63fc
Alexander Godlewski <alex@implode.ca>
2011-04-17 21:32:09 -0400

    Added comments and docstrings to backend.py

--- src/backends/backend.py
+++ src/backends/backend.py
@@ -6,8 +6,14 @@ from PyQt4.QtNetwork import QNetworkAccessManager, QNetworkRequest
 from src.storage import storage
 
 class backend:
+    ''' The backend manages the local and remote interface to
+            repositories of show data
+    '''
+    
     def __init__(self, settings):
         self.__settings = settings
+        
+        # Set up storage based on the backend instance
         self._storage = storage(os.path.join(self.__settings.path(), self.__class__.__name__))
         
     def _setsetting(self, key, value):
@@ -20,6 +26,10 @@ class backend:
         return self.__settings.remove(self.__class__.__name__, key)
     
     def _request(self, url):
+        ''' Request data from url
+            url is str
+        '''
+        
         nam = QNetworkAccessManager()
         reply = nam.get(QNetworkRequest(QUrl(url)))
                 
@@ -29,12 +39,18 @@ class backend:
         
         redirect = reply.attribute(QNetworkRequest.RedirectionTargetAttribute)
         
+        # Follow redirects
         if redirect.type() == QVariant.Url:
             return self._request(unicode(redirect.toUrl().resolved(QUrl('')).toString()))
 
         return reply.readAll()
 
     def _download(self, url, relativepath):
+        ''' Download from url and save to relativepath
+            url is str
+            relativepath is str
+        '''
+        
         data = self._request(url)
         if len(data):
             self._storage.savedata(relativepath, data)
@@ -48,6 +64,13 @@ class backend:
         return rval
 
     def setwatched(self, value, showid, seasonid, episodeid):
+        ''' Set watched for a show or remove it 
+            value is bool
+            showid is str
+            seasonid is str
+            episodeid is str
+        '''
+        
         if value:
             self._setsetting('watched/%s/%s/%s' % (showid, seasonid, episodeid), value)
         else:
@@ -58,22 +81,50 @@ class backend:
     
     
     def searchshow(self, name):
+        ''' Search for a show and return a list of src.dataclasses.show
+            name is str
+        '''
+        
         raise NotImplementedError()
     
     def getlocalshows(self):
+        ''' Return a list local shows as src.dataclasses.show '''
+        
         raise NotImplementedError()
 
     def addshow(self, id):
+        ''' Add a show locally
+            id is str
+        '''
+        
         raise NotImplementedError()
     
     def updateshow(self, id):
+        ''' Update show with the passed id
+            id is str
+        '''
+        
         raise NotImplementedError()
     
     def getlocalseasons(self, id):
+        ''' Return a list of local src.dataclasses.season with showid id
+            id is str
+        '''
+        
         raise NotImplementedError()
     
     def getlocalepisodes(self, showid, seasonid):
+        ''' Return a list of local src.dataclasses.episode with 
+                show id showid and season id seasonid
+            showid is str
+            seasonid is str
+        '''
+        
         raise NotImplementedError()
     
     def removeshow(self, id):
+        ''' Remove local show with showid id
+            id is str
+        '''
+        
         raise NotImplementedError()
