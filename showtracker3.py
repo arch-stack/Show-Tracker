147552c1e826816195000378b785e631c0d54bf0
Alexander Godlewski <alex@implode.ca>
2011-04-22 15:13:42 -0400

    Changed the main py to better support profiling

--- showtracker3.py
+++ showtracker3.py
@@ -2,9 +2,12 @@ from PyQt4 import QtGui
 import sys
 from src.mainui import mainui
 
-if __name__ == '__main__':
+def main():
     app = QtGui.QApplication(sys.argv)
     ui = mainui()
     ui.show()
     ui.load()
-    sys.exit(app.exec_())
+    return app.exec_()
+    
+if __name__ == '__main__':
+    sys.exit(main())
